ab6385a82c1a58bec403042286518acef037ce77
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-06-15 19:21:36 +1000

    Extended MST to support constraint-based binding of variables.

--- src/MagicSet.h
+++ src/MagicSet.h
@@ -40,18 +40,118 @@ class AstTranslationUnit;
 class BindingStore {
 public:
     BindingStore(const AstClause* clause) {
-        
+        generateBindingDependencies(clause);
+        reduceDependencies();
     }
 
     void bindVariable(std::string varName) {
         boundVariables.insert(varName);
+        reduceDependencies();
     }
 
-    bool isBound(std::string varName) {
+    bool isBound(std::string varName) const {
         return contains(boundVariables, varName);
     }
+
+    const std::set<std::string>& getBoundVariables() const {
+        return boundVariables;
+    }
+
 private:
     std::set<std::string> boundVariables{};
+    std::map<std::string, std::set<std::set<std::string>>> bindingDependencies{};
+
+    void generateBindingDependencies(const AstClause* clause) {
+        auto addBindingDependency = [&](std::string variable, std::set<std::string> dependency) {
+            if (contains(bindingDependencies, variable)) {
+                bindingDependencies[variable].insert(dependency);
+            } else {
+                std::set<std::set<std::string>> dependencies;
+                dependencies.insert(dependency);
+                bindingDependencies[variable] = dependencies;
+            }
+        };
+
+        visitDepthFirst(*clause, [&](const AstBinaryConstraint& bc) {
+            if (bc.getOperator() == BinaryConstraintOp::EQ) {
+                if (auto* var = dynamic_cast<AstVariable*>(bc.getLHS())) {
+                    std::set<std::string> subVars;
+                    visitDepthFirst(*bc.getRHS(),
+                            [&](const AstVariable& subVar) { subVars.insert(subVar.getName()); });
+                    addBindingDependency(var->getName(), subVars);
+                }
+                if (auto* var = dynamic_cast<AstVariable*>(bc.getRHS())) {
+                    std::set<std::string> subVars;
+                    visitDepthFirst(*bc.getLHS(),
+                            [&](const AstVariable& subVar) { subVars.insert(subVar.getName()); });
+                    addBindingDependency(var->getName(), subVars);
+                }
+            }
+        });
+    }
+
+    bool reduceDependencies() {
+        bool changed = false;
+        std::map<std::string, std::set<std::set<std::string>>> newBindingDependencies;
+        std::set<std::string> variablesToBind;
+
+        for (auto& pair : bindingDependencies) {
+            auto headVar = pair.first;
+            if (contains(boundVariables, headVar)) {
+                // No need to add the dependencies of already-bound variables
+                changed = true;
+                continue;
+            }
+
+            const auto& dependencies = pair.second;
+            assert(!dependencies.empty() &&
+                    "a variable is only added if it appears in >= 1 binary constraint");
+
+            bool nowBound = false;
+            std::set<std::set<std::string>> newDependencies;
+            for (const auto& dep : dependencies) {
+                if (dep.empty()) {
+                    // Dependency satisfied!
+                    nowBound = true;
+                    changed = true;
+                    break;
+                }
+
+                // Only keep unbound variables in the dependency
+                std::set<std::string> newDependency;
+                for (const auto& var : dep) {
+                    if (!contains(boundVariables, var)) {
+                        newDependency.insert(var);
+                    } else {
+                        changed = true;
+                    }
+                }
+
+                newDependencies.insert(newDependency);
+            }
+
+            if (nowBound) {
+                // Dependency has been satisfied
+                variablesToBind.insert(headVar);
+            } else {
+                // Dependencies not satisfied yet, keep them in store
+                newBindingDependencies[headVar] = newDependencies;
+            }
+        }
+
+        for (auto var : variablesToBind) {
+            boundVariables.insert(var);
+        }
+
+        if (changed) {
+            bindingDependencies = newBindingDependencies;
+            reduceDependencies();
+            return true;
+        }
+
+        assert(bindingDependencies == newBindingDependencies && "unexpected change");
+        return false;
+    }
 };
 
 class AdornedPredicate {
