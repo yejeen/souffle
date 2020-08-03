b4de1de3e047acb0473449c51e058fb020d34ca0
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2020-08-04 09:10:19 +1000

    Reformatting

--- src/tests/ast_parser_utils_test.cpp
+++ src/tests/ast_parser_utils_test.cpp
@@ -16,10 +16,10 @@
 
 #include "tests/test.h"
 
+#include "ParserUtils.h"
 #include "ast/Atom.h"
 #include "ast/Clause.h"
 #include "ast/Node.h"
-#include "ParserUtils.h"
 #include "utility/ContainerUtil.h"
 #include "utility/StringUtil.h"
 #include <memory>
