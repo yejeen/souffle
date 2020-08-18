96c851a4f7599ae12d03186b17f9c59fcc98b0e1
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-08-18 17:06:07 +1000

    Added fileheaders and fixed includes.

--- src/ast/analysis/ClauseNormalisation.cpp
+++ src/ast/analysis/ClauseNormalisation.cpp
@@ -1,3 +1,19 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2020, The Souffle Developers. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
+/************************************************************************
+ *
+ * @file ClauseNormalisation.cpp
+ *
+ * Defines functionality for classes related to clause normalisation
+ *
+ ***********************************************************************/
+
 #include "ast/analysis/ClauseNormalisation.h"
 #include "ast/Aggregator.h"
 #include "ast/Atom.h"
--- src/ast/analysis/ClauseNormalisation.h
+++ src/ast/analysis/ClauseNormalisation.h
@@ -1,8 +1,26 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2020, The Souffle Developers. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
+/************************************************************************
+ *
+ * @file ClauseNormalisation.h
+ *
+ * Defines a clause-normalisation analysis, providing a normal form for
+ * each clause in the program.
+ *
+ ***********************************************************************/
+
 #pragma once
 
 #include "ast/QualifiedName.h"
 #include "ast/analysis/Analysis.h"
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -12,7 +30,6 @@ class AstArgument;
 class AstAtom;
 class AstClause;
 class AstLiteral;
-class AstQualifiedName;
 class AstTranslationUnit;
 
 class NormalisedClause {
