473f47fe7e1380cdccef5b94faa95d2f7d228299
Brian Fairservice <bfairservice@grammatech.com>
2020-08-31 16:27:32 -0400

    Run vcpkg in cmd

---
+++ install-get-opt-msvc.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/microsoft/vcpkg
+call .\vcpkg\bootstrap-vcpkg.bat
+vcpkg\vcpkg install getopt:x64-windows
+setx /M PATH "%PATH$%;%cd%\vcpkg\installed\x64-windows\bin"
+setx /M INCLUDE "%INCLUDE%;%cd%\vcpkg\installed\x64-windows\include;"
+setx /M LIB "%LIB%;%cd%\vcpkg\installed\x64-windows\lib"
+refreshenv
