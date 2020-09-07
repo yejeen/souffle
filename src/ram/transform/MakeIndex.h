982a6b78ede07fafe2c3c0844a7ed8d7de26a042
Sam <sarc9328@plang8.cs.usyd.edu.au>
2020-09-07 22:22:41 +1000

    Added proper rewrite transformation for strict inequalities

--- src/ram/transform/MakeIndex.h
+++ src/ram/transform/MakeIndex.h
@@ -79,10 +79,7 @@ public:
      */
     using ExpressionPair = std::pair<std::unique_ptr<RamExpression>, std::unique_ptr<RamExpression>>;
 
-    ExpressionPair getSignedExpressionPair(const RamConstraint* binRelOp, size_t& element, int identifier);
-    ExpressionPair getUnsignedExpressionPair(const RamConstraint* binRelOp, size_t& element, int identifier);
-    ExpressionPair getFloatExpressionPair(const RamConstraint* binRelOp, size_t& element, int identifier);
-
+    ExpressionPair getExpressionPair(const RamConstraint* binRelOp, size_t& element, int identifier);
     ExpressionPair getLowerUpperExpression(RamCondition* c, size_t& element, int level);
 
     /**
