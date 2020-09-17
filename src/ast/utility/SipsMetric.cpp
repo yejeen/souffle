b42b560c3e70c47830229df40f4dcec6ff18d0a1
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-09-17 19:14:22 +1000

    Added SipsMetric class.
    Not used or populated yet.

---
+++ src/ast/utility/SipsMetric.cpp
@@ -0,0 +1,20 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2020, The Souffle Developers. All rights reserved
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
+/************************************************************************
+ *
+ * @file SipsMetric.cpp
+ *
+ * Defines the SipsMetric class, which specifies cost functions for atom orderings in a clause.
+ *
+ ***********************************************************************/
+
+#include "ast/utility/SipsMetric.h"
+#include "ast/utility/BindingStore.h"
+
+namespace souffle::ast {};  // namespace souffle::ast
---
+++ src/ast/utility/SipsMetric.h
@@ -0,0 +1,54 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2020, The Souffle Developers. All rights reserved
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
+/************************************************************************
+ *
+ * @file SipsMetric.h
+ *
+ * Defines the SipsMetric class, which specifies cost functions for atom orderings in a clause.
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include <vector>
+
+namespace souffle::ast {
+
+class Atom;
+class BindingStore;
+class Clause;
+
+class SipsMetric {
+public:
+    std::vector<size_t> getReordering(const Clause* clause) const;
+
+protected:
+    virtual std::vector<float> evaluateCosts(
+            const std::vector<Atom*> atoms, const BindingStore& bindingStore) const = 0;
+};
+
+class StrictSips : public SipsMetric {
+protected:
+    std::vector<float> evaluateCosts(
+            const std::vector<Atom*> atoms, const BindingStore& bindingStore) const override;
+};
+
+class InputSips : public SipsMetric {
+protected:
+    std::vector<float> evaluateCosts(
+            const std::vector<Atom*> atoms, const BindingStore& bindingStore) const override;
+};
+
+class ProfileUseSips : public SipsMetric {
+protected:
+    std::vector<float> evaluateCosts(
+            const std::vector<Atom*> atoms, const BindingStore& bindingStore) const override;
+};
+
+};  // namespace souffle::ast
